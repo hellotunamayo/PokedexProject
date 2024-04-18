5a25f13c95bd8da894943372900834c307288ea4
Min Yoo <artfrige@gmail.com>
2024-04-18 21:21:29 +0900

    Initial Commit

---
+++ PokedexProject/PokedexProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexProjectApp.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
