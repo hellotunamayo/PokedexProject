5386a935510f04ad903903294aaea6f81bf7a134
jinwoong <baseball0020@gmail.com>
2024-04-24 01:06:28 +0900

    feat: `Local` 열거형 추가

---
+++ PokedexProject/Models/Locale.swift
@@ -0,0 +1,33 @@
+//
+//  Locale.swift
+//  PokedexProject
+//
+//  Created by jinwoong Kim on 4/24/24.
+//
+
+import Foundation
+
+enum Locale: String, Identifiable {
+    var id: Self { self }
+    
+    case ko
+    case jp
+    case cn
+    case de
+    case en
+    
+    var accessName: String {
+        switch self {
+            case .ko:
+                return "ko"
+            case .jp:
+                return "ja-Hrkt"
+            case .cn:
+                return "zh-Hant"
+            case .de:
+                return "de"
+            case .en:
+                return "en"
+        }
+    }
+}
