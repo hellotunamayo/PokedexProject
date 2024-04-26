a68cb8fc976759e6c87698707e111ba3137461fe
Min Yoo <artfrige@gmail.com>
2024-04-26 23:12:50 +0900

    add: Extra List View, GitHubView 추가

---
+++ PokedexProject/View/ExtraView/ExtraViewList.swift
@@ -0,0 +1,62 @@
+//
+//  ExtraViewList.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/26/24.
+//
+
+import SwiftUI
+
+struct ExtraViewList: View {
+    
+    @State private var copyrightText: String = """
+Pokémon is a trademark and copyright of Nintendo Inc., Game Freak, and Creatures. This application, including but not limited to its design, images, and information, is provided for informational purposes only and is not affiliated with or endorsed by Nintendo Inc., Game Freak, or Creatures. All Pokémon-related content used in this application is used under fair use principles for educational and informational purposes only. Pokémon and Pokémon character names are trademarks of Nintendo Inc.
+"""
+    
+    var body: some View {
+        NavigationStack {
+            HStack {
+                Image("pokedexIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24, alignment: .center)
+                
+                Text("PokeDex Project")
+                    .font(Font.custom("Silkscreen-Regular", size: 24))
+            }
+            
+            List {
+                Section("Legal Information") {
+                    NavigationLink("Copyright Notice") {
+                        ScrollView {
+                            VStack {
+                                Text("Copyright Notice")
+                                    .fontWeight(.bold)
+                                    .font(.title)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.bottom, 10)
+                                Text(copyrightText)
+                                    .lineLimit(nil)
+                                    .lineSpacing(6.0)
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                
+                Section("Software License") {
+                    Text("Apache 2.0 License")
+                }
+                
+                Section("software version") {
+                    Text("0.1 - For user test only")
+                }
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    ExtraViewList()
+}
---
+++ PokedexProject/View/ExtraView/GitHubView.swift
@@ -0,0 +1,18 @@
+//
+//  GitHubView.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/26/24.
+//
+
+import SwiftUI
+
+struct GitHubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitHubView()
+}
