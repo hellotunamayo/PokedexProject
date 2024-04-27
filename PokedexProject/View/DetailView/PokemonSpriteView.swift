ab4ad40973f8109e1bdfb815de11383b8fed294e
Min Yoo <artfrige@gmail.com>
2024-04-27 18:31:01 +0900

    design: 포켓몬 상세페이지 스프라이트 뷰에 시차효과 적용

--- PokedexProject/View/DetailView/PokemonSpriteView.swift
+++ PokedexProject/View/DetailView/PokemonSpriteView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct PokemonSpriteView: View {
+    
+    @State private var roll: Double = 0.0
+    @State private var pitch: Double = 0.0
+    
+    let motionManager: CMMotionManager = CMMotionManager()
     let imageUrl: URL
     let viewWidth: CGFloat
     var body: some View {
@@ -17,6 +23,17 @@ struct PokemonSpriteView: View {
                     .fill(Color(uiColor: UIColor(red: 213/255, green: 50/255, blue: 50/255, alpha: 0.3)))
                     .padding(.top, viewWidth / 2)
                 image
+                    .offset(x: roll * 10, y: pitch * 10 - 5)
+                    .onAppear {
+                        if motionManager.isDeviceMotionAvailable {
+                            motionManager.deviceMotionUpdateInterval = 0.01
+                            motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
+                                guard let motion = motion else { return }
+                                roll = motion.attitude.roll
+                                pitch = motion.attitude.pitch
+                            }
+                        }
+                    }
             }
         } placeholder: {
             Rectangle()
