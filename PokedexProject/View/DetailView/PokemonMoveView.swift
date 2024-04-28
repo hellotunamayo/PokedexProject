043c64dca27e892ddbc0b5bc575eaf41633eab9f
Min Yoo <artfrige@gmail.com>
2024-04-28 16:47:47 +0900

    create: Pokemon moveset 뷰 분리

---
+++ PokedexProject/View/DetailView/PokemonMoveView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonMoveView.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/28/24.
+//
+
+import SwiftUI
+
+struct PokemonMoveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonMoveView()
+}
