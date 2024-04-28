e2ed800f8a6cafe82269c02c07635f8cbdb7d2c8
jinwoong <baseball0020@gmail.com>
2024-04-28 14:12:37 +0900

    feat: configure `Requestable` protocol for http request

--- PokedexProject/Network/Protocols/Requestable.swift
+++ PokedexProject/Network/Protocols/Requestable.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol Requestable {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HttpMethod { get }
+    var headers: [HttpHeader]? { get }
+    var body: Data? { get }
+}
+
+extension Requestable {
+    private func url() throws -> URL {
+        guard let url = URL(string: baseURL) else {
+            throw URLError(.badURL)
+        }
+        return url.appending(path: path)
+    }
+    
+    func buildRequest() throws -> URLRequest {
+        let url = try url()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        headers?.forEach { request.addValue($0.value, forHTTPHeaderField: $0.key) }
+        
+        return request
+    }
+}
