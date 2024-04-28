e81077668562e0f84e6b9644f94e4d627ebd01b9
jinwoong <baseball0020@gmail.com>
2024-04-28 14:04:14 +0900

    feat: define `HttpHeader` for http request

--- PokedexProject/Network/Model/HttpHeader.swift
+++ PokedexProject/Network/Model/HttpHeader.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum HttpHeader {
+    case accept(MIMEType)
+    case contentType(MIMEType)
+    
+    var key: String {
+        switch self {
+            case .accept:
+                return "Accept"
+            case .contentType:
+                return "Content-Type"
+        }
+    }
+    
+    var value: String {
+        switch self {
+            case let .accept(mimeType):
+                return mimeType.headerValue
+            case let .contentType(mimeType):
+                return mimeType.headerValue
+        }
+    }
+}
