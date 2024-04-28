2b78e68130dcef66ad1ebe27bff3bd526cdba1eb
jinwoong <baseball0020@gmail.com>
2024-04-28 15:17:29 +0900

    feat: configure basic `PokemonAPIService`

--- PokedexProject/Network/Impl/PokemonAPIService.swift
+++ PokedexProject/Network/Impl/PokemonAPIService.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+final class PokemonAPIService: APIService {
+    var session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
