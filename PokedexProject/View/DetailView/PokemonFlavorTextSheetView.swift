f17501fe56b23ed763838b082cfefc9955728c56
Min Yoo <artfrige@gmail.com>
2024-04-29 22:11:30 +0900

    create: 플레이버 텍스트 시트뷰 파일 분리

---
+++ PokedexProject/View/DetailView/PokemonFlavorTextSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonFlavorTextSheetView.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/29/24.
+//
+
+import SwiftUI
+
+struct PokemonFlavorTextSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonFlavorTextSheetView()
+}
