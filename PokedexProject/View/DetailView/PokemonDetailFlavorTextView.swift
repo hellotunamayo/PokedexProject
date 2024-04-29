dd826f1370113356300aa3032f99317789b14947
Min Yoo <artfrige@gmail.com>
2024-04-29 20:08:13 +0900

    create: 플레이버 텍스트 뷰 생성

---
+++ PokedexProject/View/DetailView/PokemonDetailFlavorTextView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailFlavorTextView.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 4/29/24.
+//
+
+import SwiftUI
+
+struct PokemonDetailFlavorTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetailFlavorTextView()
+}
