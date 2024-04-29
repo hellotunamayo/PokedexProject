343110cfad184c391d5dd37b2ae0af49668d56fc
Min Yoo <artfrige@gmail.com>
2024-04-29 22:11:44 +0900

    refactor: 플레이버 텍스트 시트뷰 작성

--- PokedexProject/View/DetailView/PokemonFlavorTextSheetView.swift
+++ PokedexProject/View/DetailView/PokemonFlavorTextSheetView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct PokemonFlavorTextSheetView: View {
+    
+    @State private var showingFlavorText: [FlavorText] = []
+    @Binding var locale: Locale?
+    
+    let flavorText: [FlavorText]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(showingFlavorText) { text in
+                Text(text.version.name)
+                Text(getEmptySpaceTrimmed(string: text.flavorText))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .onAppear {
+            getFlavorTextByLocale()
+        }
+        .onDisappear {
+            locale = nil
+        }
+    }
+    
+    func getFlavorTextByLocale() {
+        switch locale {
+            case .en:
+                showingFlavorText = flavorText.filter { $0.language.name == "en" }
+            case .ko:
+                showingFlavorText = flavorText.filter { $0.language.name == "ko" }
+            case .jp:
+                showingFlavorText = flavorText.filter { $0.language.name == "ja" }
+            case .cn:
+                showingFlavorText = flavorText.filter { $0.language.name == "zh-Hant" }
+            case .de:
+                showingFlavorText = flavorText.filter { $0.language.name == "de" }
+            case .none:
+                showingFlavorText = flavorText.filter { $0.language.name == "en" }
+        }
+    }
+    
+    func getEmptySpaceTrimmed(string: String) -> String {
+        let setString = string.replacingOccurrences(of: "\n", with: " ")
+        return setString
     }
 }
 
 #Preview {
-    PokemonFlavorTextSheetView()
+    PokemonFlavorTextSheetView(locale: .constant(.jp), flavorText: [FlavorText(flavorText: "test string", language: FlavorTextLanguage(name: "ja", url: ""), version: FlavorTextVersion(name: "red", url: ""))])
 }
