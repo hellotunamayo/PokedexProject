4039d1be09adc0d3ba8c730331b96bba7d46f83a
Min Yoo <artfrige@gmail.com>
2024-05-04 23:24:40 +0900

    create: 즐겨찾기 모델과 뷰 추가

---
+++ PokedexProject/Models/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteModel.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 5/4/24.
+//
+
+import Foundation
---
+++ PokedexProject/View/FavoriteView/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  PokedexProject
+//
+//  Created by Minyoung Yoo on 5/4/24.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
