718958ea9d99a210ae2ec26b7986c85446309e34
Min Yoo <artfrige@gmail.com>
2024-05-04 23:26:00 +0900

    refactor: 즐겨찾기 모델과 뷰 수정

--- PokedexProject/Models/FavoriteModel.swift
+++ PokedexProject/Models/FavoriteModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class FavoriteModel: Identifiable {
+    @Attribute(.unique) var id: UUID = UUID()
+    var pokemonName: String
+    var pokemonIndex: Int
+    var pokemonFrontPortraitURLString: String
+    var pokemonType: PokemonType
+    var pokemonListObject: PokemonListObject
+    
+    init(pokemonName: String, 
+         pokemonIndex: Int,
+         pokemonFrontPortraitURLString: String,
+         pokemonType: PokemonType,
+         pokemonListObject: PokemonListObject) {
+        self.pokemonName = pokemonName
+        self.pokemonIndex = pokemonIndex
+        self.pokemonFrontPortraitURLString = pokemonFrontPortraitURLString
+        self.pokemonType = pokemonType
+        self.pokemonListObject = pokemonListObject
+    }
+}
--- PokedexProject/View/FavoriteView/FavoriteView.swift
+++ PokedexProject/View/FavoriteView/FavoriteView.swift
@@ -6,10 +6,59 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FavoriteView: View {
+    @Query var favoritedPokemon: [FavoriteModel]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack {
+                Image("pokedexIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24, alignment: .center)
+                
+                Text("Favorite")
+                    .font(Font.custom("Silkscreen-Regular", size: 24))
+            }
+            
+            if favoritedPokemon.isEmpty {
+                EmptyFavoriteView()
+                    .frame(maxHeight: .infinity)
+            } else {
+                ScrollView {
+                    ForEach(favoritedPokemon) { pokemon in
+                        LazyVStack {
+                            NavigationLink {
+                                PokemonDetailView(pokeData: pokemon.pokemonListObject, endpoint: "https://pokeapi.co/api/v2/pokemon/\(pokemon.pokemonIndex)/")
+                            } label: {
+                                EntryViewCell(index: pokemon.pokemonIndex - 1, pokemonName: pokemon.pokemonName)
+                                    .frame(height: 140)
+                            }
+                            .backgroundStyle(Color("backgroundColor"))
+                        }
+                        .padding(.vertical, -4)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct EmptyFavoriteView: View {
+    var body: some View {
+        VStack{
+            Image("emptyViewCharacterImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 190, maxHeight: 190, alignment: .center)
+            Text("You haven't add\nany Pokémon!")
+                .multilineTextAlignment(.center)
+                .fontWeight(.bold)
+                .font(.title)
+                .padding(.top, -10)
+        }
     }
 }
 
