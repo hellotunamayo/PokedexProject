4bbcfe58a7f792caf21d3688acc65482f4f5b88d
Min Yoo <artfrige@gmail.com>
2024-05-04 23:26:57 +0900

    refactor: 앱에 즐겨찾기 데이터 SwiftData 모델 컨테이너 추가

--- PokedexProject/PokedexProjectApp.swift
+++ PokedexProject/PokedexProjectApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct PokedexProjectApp: App {
@@ -13,5 +14,6 @@ struct PokedexProjectApp: App {
         WindowGroup {
             MainView()
         }
+        .modelContainer(for: FavoriteModel.self)
     }
 }
